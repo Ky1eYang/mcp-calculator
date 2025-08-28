50e1dd2df672be5ebb713d9e7bd3c8c4f2b8a5ac
yangkaiyue <yangkaiyue1@tenclass.com>
2025-08-28 11:00:23 +0800

    feat: 完善一下示例代码的文档部分

--- examples/async_publish_future.py
+++ examples/async_publish_future.py
@@ -1,4 +1,8 @@
-# server.py
+"""
+示例: 异步发布消息
+- 演示如何使用异步函数向客户端发布消息
+注意需要类似示例完整定义各个函数的描述和参数Field描述, 确保FastMCP能正确解析工具内容并发送给LLM
+"""
 from fastmcp import FastMCP
 from typing import Union, List
 from pydantic import Field
@@ -6,7 +10,7 @@ import asyncio
 import logging
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("AsyncAction")
 logger.addHandler(logging.StreamHandler())
 
 # Create an MCP server
@@ -16,7 +20,7 @@ mcp = FastMCP("AsyncAction", host="0.0.0.0", port=9999)
 async def async_action(topics: Union[List[str], str], message: str, system_args: dict) -> str:
     """模拟异步操作, 例如向设备发送消息"""
     client_id = system_args.get("client_id", "unknown_client")
-    await asyncio.sleep(10)  # 模拟异步延迟
+    await asyncio.sleep(5)  # 模拟异步延迟
     logger.info(f"Sent Message '{message}' to client:{client_id} on topic:{topics} successfully.")
 
 
@@ -29,9 +33,9 @@ async def send_message(
     """
     向客户端异步发送主题/消息
     """
-    # asyncio.run_coroutine_threadsafe(async_action(topics, message, system_args), loop) # 在子线程事件循环中运行协程
     loop = asyncio.get_event_loop()
     loop.create_task(async_action(topics, message, system_args))  # 在当前事件循环中运行协程
+    logger.info("Scheduling task and return immediately.")
     return "Message is being sent asynchronously."
 
 
